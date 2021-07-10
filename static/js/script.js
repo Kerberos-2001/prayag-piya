e08f22beee1e137cf40f68c54dedc5942af4502f
prayag piya <peanutmonster@prayags-MacBook-Air.local>
2021-07-10 16:18:08 +0545

    Adding date

--- static/js/script.js
+++ static/js/script.js
@@ -8,23 +8,5 @@ function editDOM(){
 
 setTimeout(editDOM, 3000);
 
-// var layers = document.getElementsByClassName('layer')
-// var numLayer = layers.length
-// var count  = [0, 10]
-// animationClass = ['one', 'two', 'four', 'five']
-
-// for(var i =0; i <= numLayer-1; i++)
-// {
-//     layers[i].classList.add(animationClass[i])
-//     if (i == 2 && numLayer == 2)
-//     {
-//         break
-//     }
-//     else{
-//         powerValue = Math.pow(i+count[i], 2) 
-//         console.log(powerValue+'px')
-//         layers[i].style.marginLeft = powerValue+'px'
-//         layers[numLayer-1].style.marginLeft = powerValue+'px'
-//         numLayer -= 1;
-//     }
-// }
+const d = new Date();
+document.getElementById('date').innerHTML = d.toDateString() + '<br> (ready to work)';
